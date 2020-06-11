743b9aba7d3c58f8c4a9cc969ae6075eba89c670
blueskyletters <blueskyfairblogger@gmail.com>
2020-06-11 18:44:30 +0530

    setting up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/layouts/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      My app
+    </div>
+  );
+}
+
+export default App;
